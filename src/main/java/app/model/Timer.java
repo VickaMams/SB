f0599451cd64b23a1fa46884c62f29438060f40b
Victoria2412 <nado122012@gmail.com>
2022-08-30 22:42:02 +0300

    may I?

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,14 @@
 package app.model;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope ("prototype")
 public class Timer {
+    public Timer(){
+        System.out.println("Timer bean is created");
+    }
 
     private Long nanoTime = System.nanoTime();
 
